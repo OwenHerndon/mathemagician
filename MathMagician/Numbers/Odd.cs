8479c3b68ed028b66da718a0baca3e6474cb68c6
OwenHerndon <owen.w.herndon@gmail.com>
2017-01-28 11:12:17 -0800

    odd test and methods

---
+++ MathMagician/Numbers/Odd.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathMagician.Numbers
+{
+    public class Odd : Integer
+    {
+        private int First { get; set; }
+
+        public Odd()
+        {
+            First = 1;
+        }
+
+        public int GetFirst()
+        {
+            return First;
+        }
+
+        public int GetNext(int current)
+        {
+            current = current + 2;
+            return current;
+        }
+
+        public int[] GetSequence(int how_many)
+        {
+            int[] sequence = new int[how_many];
+            sequence[0] = GetFirst(); //i = 1
+            for (int i = 1; i < sequence.Length; i++)
+            {
+                sequence[i] = GetNext(sequence[i - 1]);
+            }
+            return sequence;
+        }
+
+        public string PrintNumbers(int[] sequence_array)
+        {
+            return String.Join(" ", sequence_array);
+        }
+    }
+}
---
+++ MathMagicianTests/Numbers/OddTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathMagician.Numbers;
+
+namespace MathMagicianTests.Numbers
+{
+    [TestClass]
+    public class OddTest
+    {
+        [TestMethod]
+        public void OddInstance()
+        {
+            Odd oddNumber = new Odd();
+
+            Assert.IsNotNull(oddNumber);
+        }
+
+        [TestMethod]
+        public void OddFirst()
+        {
+            //Arrange(where you set stuff up)
+            Odd oddNumber = new Odd();
+
+            //Act(call method being tested)
+            int expectedResult = 1;
+            int actualResult = oddNumber.GetFirst();
+
+            //Assert(check the output from method)
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [TestMethod]
+        public void OddNextTest()
+        {
+            //arrange
+            Odd oddNumber = new Odd();
+
+            //act
+            int expectedResult = 3;
+            int actualResult = oddNumber.GetNext(1)
+;
+            //assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [TestMethod]
+        public void EvenSequenceTest()
+        {
+            //arrange
+            Odd oddNumber = new Odd();
+
+            //act
+            int[] expectedResult = { 1, 3, 5, 7, 9 };
+            int[] actualResult = oddNumber.GetSequence(5);
+
+            //assert
+            Assert.AreEqual(expectedResult.Length, actualResult.Length);
+            CollectionAssert.AreEqual(expectedResult, actualResult);
+
+        }
+        [TestMethod]
+        public void OddPrintTest()
+        {
+            //arrange
+            Odd oddNumber = new Odd();
+
+            //act
+            string expectedResult = "1 3 5 7 9";
+            int[] inputArray = new[] { 1, 3, 5, 7, 9 };
+            string actualResult = oddNumber.PrintNumbers(inputArray);
+
+            //assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}

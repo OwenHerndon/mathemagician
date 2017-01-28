d205af213eb3182b0a15314b7baf31894c736c02
OwenHerndon <owen.w.herndon@gmail.com>
2017-01-28 11:58:57 -0800

    Began fibonacci

---
+++ MathMagician/Numbers/EvenSmaller.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathMagician.Numbers
+{
+    class EvenSmaller : NaturalNumber
+    {
+        //change first to protected var in nat numbers
+        //change getnext to virtual public int
+
+        //* add property of protected int step{get set}
+        //* Step = 1; in naturalnumber constructor
+        
+        //public EvenSmaller()
+        //{
+        //  First = 2;
+        //* Step = 2 added with step property
+        //}
+
+
+        //---Below can be removed when step is added to program---
+        //public override int GetNext(int current)
+        //{
+        //  return current + 2;
+        //}
+    }
+}
---
+++ MathMagicianTests/Numbers/FibonacciTest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathMagician.Numbers;
+
+namespace MathMagicianTests.Numbers
+{
+    [TestClass]
+    public class FibonacciTest
+    {   
+        // F3 = F2 + F1
+        // F7 = F6 + F5
+        [TestMethod]
+        public void FiboInstance()
+        {
+            Fibonacci fiboNumber = new Fibonacci();
+
+            Assert.IsNotNull(fiboNumber);
+        }
+        [TestMethod]
+        public void FiboFirst()
+        {
+            //Arrange(where you set stuff up)
+            Fibonacci fiboNumber = new Fibonacci();
+
+            //Act(call method being tested)
+            int expectedResult = 1;
+            int actualResult = fiboNumber.GetFirst();
+
+            //Assert(check the output from method)
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [TestMethod]
+        public void FiboNextTest()
+        {
+            //arrange
+            Fibonacci fiboNumber = new Fibonacci();
+
+            //act
+            int expectedResult = 2;
+            int actualResult = fiboNumber.GetNext(1)
+;
+            //assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [TestMethod]
+        public void NatSequenceTest()
+        {
+            //arrange
+            Fibonacci fiboNumber = new Fibonacci();
+
+            //act
+            int[] expectedResult = { 1, 1,2, 3, 5 };
+            int[] actualResult = fiboNumber.GetSequence(5);
+
+            //assert
+            Assert.AreEqual(expectedResult.Length, actualResult.Length);
+            CollectionAssert.AreEqual(expectedResult, actualResult);
+
+        }
+        [TestMethod]
+        public void NatPrintTest()
+        {
+            //arrange
+            Fibonacci fiboNumber = new Fibonacci();
+
+            //act
+            string expectedResult = "1 1 2 3 5";
+            int[] inputArray = new[] { 1, 1, 2, 3, 5 };
+            string actualResult = fiboNumber.PrintNumbers(inputArray);
+
+            //assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
